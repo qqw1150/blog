ec4e64570a826001a0af535649c7e425e2e4908c
zhangyong <1452184808@qq.com>
2019-11-26 11:20:48 +0800

    add tag

--- app/services/ArticleService.php
+++ app/services/ArticleService.php
@@ -12,6 +12,7 @@ namespace app\services;
 use app\models\Article;
 use app\models\domains\ArticleForm;
 use app\models\Tag;
+use Phalcon\Db\Adapter\Pdo\Mysql;
 use Phalcon\Logger\Adapter\File;
 
 class ArticleService extends BaseService
@@ -20,9 +21,16 @@ class ArticleService extends BaseService
      * 保存文章（创建或修改）
      * @param ArticleForm $articleForm
      * @return bool
+     * @throws \Phalcon\Crypt\Mismatch
      */
     public function save($articleForm)
     {
+        /**
+         * @var Mysql $db
+         */
+        $db=$this->di->get('db');
+        $db->begin();
+
         if ($articleForm->getId()) {
             $article = Article::findFirst($articleForm->getId());
         } else {
@@ -43,8 +51,29 @@ class ArticleService extends BaseService
         $article->setUserId($user['id']);
         if ($article->save() === false) {
             $this->recordErr($article);
+            $db->rollback();
             return false;
         }
+
+        /**
+         * 添加标签
+         * @var TagService $tagService
+         */
+        $tagService = $this->di->get('tagService');
+        $tagNames=$articleForm->getTags();
+        foreach ($tagNames as $tagName){
+            $b=$tagService->addUserTag($tagName,$user['id']);
+            if($b===false){
+                $db->rollback();
+            }
+
+            $b=$tagService->addArticleTag($tagName,$article->getId());
+            if($b===false){
+                $db->rollback();
+            }
+        }
+
+        $db->commit();
 
         return true;
     }
